1c387886cc056c79512b461be2437a31ee5a1d50
nik-knack <dlm4695@rit.edu>
2024-11-12 09:15:45 -0500

    continue circle blast hw

---
+++ HW-pixi-game-start/js/main.js
@@ -0,0 +1,75 @@
+// We will use `strict mode`, which helps us by having the browser catch many common JS mistakes
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+"use strict";
+const app = new PIXI.Application();
+
+// aliases
+let stage;
+let assets;
+
+let sceneWidth, sceneHeight;
+
+// game variables
+let startScene;
+let gameScene, ship, scoreLabel, lifeLabel, shootSound, hitSound, fireballSound;
+let gameOverScene;
+
+let circles = [];
+let bullets = [];
+let aliens = [];
+let explosions = [];
+let explosionTextures;
+let score = 0;
+let life = 100;
+let levelNum = 1;
+let paused = true;
+
+// Load all assets
+loadImages();
+
+async function loadImages() {
+    // https://pixijs.com/8.x/guides/components/assets#loading-multiple-assets
+    PIXI.Assets.addBundle("sprites", {
+        spaceship: "images/spaceship.jpg",
+        explosions: "images/explosions.jpg",
+        move: "images/move.jpg",
+    });
+
+    // The second argument is a callback function that is called whenever the loader makes progress.
+    assets = await PIXI.Assets.loadBundle("sprites", (progress) => {
+        console.log(`progress=${(progress * 100).toFixed(2)}%`); // 0.4288 => 42.88%
+    });
+
+    setup();
+}
+
+async function setup() {
+    await app.init({ width: 600, height: 600 });
+
+    document.body.appendChild(app.canvas);
+
+    stage = app.stage;
+    sceneWidth = app.renderer.width;
+    sceneHeight = app.renderer.height;
+
+    // #1 - Create the `start` scene
+
+    // #2 - Create the main `game` scene and make it invisible
+
+    // #3 - Create the `gameOver` scene and make it invisible
+
+    // #4 - Create labels for all 3 scenes
+
+    // #5 - Create ship
+
+    // #6 - Load Sounds
+
+    // #7 - Load sprite sheet
+
+    // #8 - Start update loop
+
+    // #9 - Start listening for click events on the canvas
+
+    // Now our `startScene` is visible
+    // Clicking the button calls startGame()
+}

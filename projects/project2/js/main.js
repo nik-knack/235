d9a95582d2fb26252cd5a6e967657f1c28a516b3
nik-knack <dlm4695@rit.edu>
2024-11-13 22:01:51 -0500

    updated javascript

---
+++ projects/project2/js/main.js
@@ -0,0 +1,82 @@
+"use strict";
+window.onload = init;
+let displayTerm = "";
+
+function init() {
+    document.querySelector("#search").onclick = getData;
+}
+
+function getData() {
+    // 1 - main entry point to web service
+    const SERVICE_URL = "https://www.amiiboapi.com/api/amiibo/?name=";
+
+    // No API Key required!
+
+    // 2 - build up our URL string
+    // not necessary for this service endpoint
+    let url = SERVICE_URL;
+
+    // 3 - parse the user entered term we wish to search
+    // not necessary for this service endpoint
+    let term = document.querySelector("#searchterm").value.trim();
+    displayTerm = term;
+    term = encodeURIComponent(term);
+    url += term;
+
+    // 4 - update the UI
+    document.querySelector("#debug").innerHTML = `<b>Querying web service with:</b> <a href="${url}" target="_blank">${url}</a>`;
+
+    // 5 - create a new XHR object
+    let xhr = new XMLHttpRequest();
+
+    // 6 - set the onload handler
+    xhr.onload = dataLoaded;
+
+    // 7 - set the onerror handler
+    xhr.onerror = dataError;
+
+    // 8 - open connection and send the request
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function dataError(e) {
+    console.log("An error occurred");
+}
+
+function dataLoaded(e) {
+    // 1 - e.target is the xhr object
+    let xhr = e.target;
+
+    // 2 - xhr.responseText is the JSON file we just downloaded
+    console.log(xhr.responseText);
+
+    // 3 - turn the text into a parsable JavaScript object
+    let obj = JSON.parse(xhr.responseText);
+
+    if (!obj.amiibo || obj.amiibo.length == 0) {
+        document.querySelector("#status").innerHTML = "<b>No results found for '" + term + "'<b>";
+        return; // bail out
+    }
+
+    // 4 - if there is an array of results, loop through them
+    let results = obj.amiibo;
+    console.log("results.length = " + results.length);
+    let bigString = "<p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
+
+    for (let i = 0; i < results.length; i++) {
+        let result = results[i];
+
+        let image = result.image;
+        let character = result.character;
+
+        let line = `<img src="${image}" title="${character}" />`;
+        console.log(`${character}`);
+
+        bigString += line;
+
+    }
+
+    // 5 - display final results to user
+    document.querySelector("#content").innerHTML = bigString;
+}
